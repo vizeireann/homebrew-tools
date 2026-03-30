8e513f10998dd66fb166f0ba581fd78370028a81
SG <sges@Laptop.local>
2026-03-30 16:46:07 +0100

    Added v0.1.2

--- Formula/retrnote.rb
+++ Formula/retrnote.rb
@@ -1,8 +1,8 @@
 class Retrnote < Formula
   desc "Minimalist retro-inspired HTML tag-based word processor"
   homepage "https://github.com/vizeireann/retrnote"
-  url "https://github.com/vizeireann/retrnote/releases/download/0.1.1/retrnote-macos-latest"
-  sha256 "6479df79519c86a4b7b0a85643187939b811a9d6e5ec41013461de65e64c4b21"
+  url "https://github.com/vizeireann/retrnote/releases/download/0.1.2/retrnote-macos-latest"
+  sha256 "0fcafbd302a9d7db1fcbe9ae6929bd5419a3cac8f76c7ded491325efec513867"
 
   def install
     # Install the self-contained binary
